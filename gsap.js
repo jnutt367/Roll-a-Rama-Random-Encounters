7d136f27f731af663cd477fe41dcdf978ec625de
Jason Nutt <jnutt367@gmail.com>
2021-04-16 11:36:19 -0400

    commit right before working on animating dice function is done and working

--- gsap.js
+++ gsap.js
@@ -1,3 +1,7 @@
 gsap.to(".dice", { duration: 3, rotate: 360, ease: "bounce.inOut" });
 
-gsap.to("body", { backgroundColor: "green", fontFamily: "fantasy" })
+gsap.fromTo("body", { opacity: 0 }, { opacity: 1, backgroundImage: "url(nice-pic-jason.jpg)", backgroundRepeat: "no-repeat", backgroundSize: "cover", color: "white", fontFamily: "fantasy" });
+
+gsap.to("button", { marginLeft: "25px", padding: "15px", borderRadius: "17px", cursor: "pointer" });
+
+gsap.to("h2", { duration: "4", ease: "linear", backgroundColor: "black", padding: "10px", textContent: "Now I just need to animate the dice into their shapes, easier said then done, wish me luck!", fontSize: "20px", })
