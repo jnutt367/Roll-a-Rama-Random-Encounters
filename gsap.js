0ca1e49557ca3eb6281a35465835e1ac421bb2f3
Jason Nutt <jnutt367@gmail.com>
2021-04-16 13:45:04 -0400

    commit for dice styling

--- gsap.js
+++ gsap.js
@@ -1,4 +1,14 @@
-gsap.to(".dice", { duration: 2, rotate: 360, ease: "bounce.inOut" });
+gsap.config({
+    autoSleep: 60,
+    force3D: true,
+    nullTargetWarn: false,
+    units: { left: "%", top: "%", rotation: "rad" }
+});
+
+
+
+
+gsap.to(".dice", { duration: 2, rotate: 360, ease: "bounce.inOut", force3D: true, boxShadow: "2px 5px 2px black" });
 
 gsap.fromTo("body", { opacity: 0 }, { opacity: 1, backgroundImage: "url(encounter2.jpg)", backgroundRepeat: "no-repeat", backgroundSize: "cover", color: "white", fontFamily: "fantasy" });
 gsap.to(".title", { padding: "10px", borderRadius: "15px", backgroundColor: "skyblue", opacity: "0.7", color: "blue" })
