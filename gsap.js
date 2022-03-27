51955661a44ee741e951a0bed345b92465c1f74c
Jason Nutt <jnutt367@gmail.com>
2022-03-27 17:22:03 -0400

    commit for dice roller refactor

--- gsap.js
+++ gsap.js
@@ -1,6 +1,6 @@
 gsap.to(".dice", { duration: 2, rotate: 360, ease: "elastic", force3D: true, boxShadow: "2px 5px 2px black" });
 
-gsap.fromTo("body", { opacity: 0 }, { opacity: 1, backgroundColor: "green", backgroundImage: "url(encounter2.jpg)", backgroundRepeat: "no-repeat", backgroundSize: "contain", color: "white", fontFamily: "fantasy" });
+gsap.fromTo("body", { opacity: 0 }, { opacity: 1, backgroundColor: "black", backgroundImage: "url(encounter2.jpg)", backgroundRepeat: "no-repeat", backgroundSize: "contain", color: "white", fontFamily: "fantasy" });
 gsap.to(".title", { padding: "10px", borderRadius: "15px", backgroundColor: "skyblue", opacity: "0.7", color: "blue" }, 0)
 gsap.to("button", { marginLeft: "25px", padding: "15px", borderRadius: "17px", cursor: "pointer", width: "200px" });
 
