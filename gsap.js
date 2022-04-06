ce37ad28b2ae07e1fdd019c573976b82e9c3eee8
Jason Nutt <jnutt367@gmail.com>
2022-04-05 20:35:20 -0400

    commit for best die roller layout

--- gsap.js
+++ gsap.js
@@ -1,7 +1,7 @@
-gsap.to(".dice", { duration: 2, rotate: 360, ease: "elastic", force3D: true, boxShadow: "2px 5px 2px black" });
+gsap.to(".dice", { duration: 2, rotate: 360, borderRadius: "50%", ease: "elastic", force3D: true, boxShadow: "2px 5px 2px black" });
 
-gsap.fromTo("body", { opacity: 0 }, { opacity: 1, backgroundColor: "black", backgroundImage: "url(group_photo.jpeg)", backgroundRepeat: "no-repeat", backgroundSize: "contain", color: "white", fontFamily: "fantasy" });
-gsap.to(".title", { padding: "10px", borderRadius: "15px", backgroundColor: "skyblue", opacity: "0.7", color: "blue" }, 0)
-gsap.to("button", { marginLeft: "25px", padding: "15px", borderRadius: "17px", cursor: "pointer", width: "200px" });
+gsap.fromTo("body", { opacity: 0 }, { opacity: 1, backgroundColor: "black", backgroundImage: "url(cintaur.jpeg)", backgroundRepeat: "no-repeat", backgroundSize: "contain", color: "white", fontFamily: "fantasy" });
+gsap.to(".title", { padding: "10px", borderRadius: "15px", backgroundColor: "skyblue", opacity: "0.8", color: "white" }, 0)
+gsap.to("button", { marginLeft: "25px", padding: "15px", borderRadius: "50%", cursor: "pointer", width: "200px" });
 
 gsap.to("h2", { duration: "4", ease: "linear", backgroundColor: "black", padding: "10px", fontSize: "20px", });
