ce44c6606f453ec7302deec46f006ec37be9b8ab
Jason Nutt <jnutt367@gmail.com>
2022-04-06 15:37:03 -0400

    commit for black bg and diff font family

--- gsap.js
+++ gsap.js
@@ -1,6 +1,6 @@
 gsap.to(".dice", { duration: 2, rotate: 360, borderRadius: "50%", ease: "elastic", force3D: true, boxShadow: "2px 5px 2px black" });
 
-gsap.fromTo("body", { opacity: 0 }, { opacity: 1, backgroundColor: "black", backgroundImage: "url(brian_bard.jpg)", backgroundRepeat: "no-repeat", backgroundSize: "contain", color: "white", fontFamily: "fantasy" });
+gsap.fromTo("body", { opacity: 0 }, { opacity: 1, backgroundColor: "black", backgroundImage: "url()", backgroundRepeat: "no-repeat", backgroundSize: "contain", color: "white", fontFamily: "serif" });
 gsap.to(".title", { padding: "10px", borderRadius: "15px", backgroundColor: "skyblue", opacity: "0.8", color: "white" }, 0)
 gsap.to("button", { marginLeft: "25px", padding: "15px", borderRadius: "50%", cursor: "pointer", width: "200px" });
 
